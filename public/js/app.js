64a6c84134aebcd09798d43bc2f4af64212fdcf1
Roeland Verbakel <roelandverbakel@MacBook-Pro-van-Roeland.local>
2018-03-18 17:21:33 +0100

    bug fix in safari

--- public/js/app.js
+++ public/js/app.js
@@ -387,6 +387,17 @@ $(document).ready(function() {
                   prompt: "Vul een geldig email adres in. Let op! Als je het email adres wijzigt, moet je opnieuw inloggen."
                 }
               ]
+          },
+          "geboorteDatum": {
+            identifier: "geboorteDatum",
+            optional: true,
+            rules: [
+              {
+                type: 'regExp',
+                value: "[0-9]{4}-[0-9]{2}-[0-9]{2}",
+                prompt: "Je gebruikt een non-supported browser, waarschijnlijk Safari. Datum kan hierdoor niet worden ingevuld via een datum-uitklapmenu. Vul de datum als volgt in: jjjj-mm-dd. We raden je aan om Chrome te gebruiken voor Pillars."
+              }
+            ]
           }
       }
     });  
