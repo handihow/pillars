b643ae32b10d2909b27b0f5baf7f8bb0378f5a83
Roeland Verbakel <roelandverbakel@MacBook-Pro-van-Roeland.local>
2018-04-22 16:50:50 +0200

    sorting

--- public/js/test-results-overview.js
+++ public/js/test-results-overview.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	$.get( "/user/"+ params[2] + "/api/tests", function( results ) {
 		var ctx = $('#grafiek');
-    results.sort(function(a, b){return b.created - a.created});
+    results.sort(function(a, b){return new Date(b.created) - new Date(a.created)});
 		backgroundColors = [];
 		borderColors = [];
         var labels = [];
