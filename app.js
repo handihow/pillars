c936ce8e8d48954d6e5c4a9eed99c44189c0f2f8
Roeland Verbakel <roelandverbakel@MacBook-Pro-van-Roeland.local>
2018-11-18 10:35:24 +0100

    add social login

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.deserializeUser(User.deserializeUser());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   function(accessToken, refreshToken, profile, done) {
   	User.findOne({username: { $regex: "^" + profile.emails[0].value + "$", $options: "i"}}, function (err, user) {
@@ -83,7 +83,7 @@ passport.use(new GoogleStrategy({
 passport.use(new AzureAdOAuth2Strategy({
     clientID: process.env.AZURE_CLIENT_ID,
     clientSecret: process.env.AZURE_CLIENT_SECRET,
-    callbackURL: "/auth/azureadoauth2/callback"
+    callbackURL: process.env.AZURE_CALLBACK_URL
   },
   function(accessToken, refresh_token, params, profile, done) {
   	var profile = {};
